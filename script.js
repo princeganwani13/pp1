1498a0035294e77ae798ad1fd0eb99b24fa7d981
princeganwani13 <162410380+princeganwani13@users.noreply.github.com>
2024-03-05 21:39:26 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,96 @@
+
+
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider",{
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay: 1000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".room-slider",{
+    spaceBetween: 20, 
+    grabCursor:true,
+    loop:true,
+    autoplay:{
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    }
+});
+
+var swiper = new Swiper(".gallery-slider",{
+    spaceBetween: 10, 
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+    }
+});
+
+var swiper = new Swiper(".review-slider",{
+    spaceBetween: 10, 
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+let accordions = document.querySelectorAll('.faqs .row .content .box ');
+
+accordions.forEach(acco =>{
+    acco.onclick = () =>{
+        accordions.forEach(subAcco => {subAcco.classList.remove('active')});
+        acco.classList.add('active');
+    }
+});
